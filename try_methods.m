04a13ba52976bf2b6d820d4090ae83f30334f306
Federico Poloni <fpoloni@di.unipi.it>
2018-01-25 10:59:00 +0100

    try_method for experiments in the revision

--- try_methods.m
+++ try_methods.m
@@ -4,23 +4,23 @@ relspeed = 0.01;
 maxit=10000;
 
 methods = {
-    @fixed_point, 'FixedPoint';
-    @innout, 'InnOut';
-    @newton, 'Newton';
+    @fixed_point, 'F';
+    @innout, 'IO';
+    @newton, 'N';
 %    @(a,v,R,tol) tpr_method('innout',a,v,R,tol), 
 %    @(a,v,R,tol) tpr_method('newton',a,v,R,tol),
-    @optimistic, 'Optimistic';
+    @optimistic, 'P';
 %    @optimistic_fixed_point, 
-    @optimistic_newton, 'Optimistic Newton';
-    @(a,v,R,tol) bootstrap_first_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'Bootstrap ON 1st derivative';
-    @(a,v,R,tol) bootstrap_wrong_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'Bootstrap ON wrong 1st';
-    @(a,v,R,tol) bootstrap_second_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'Bootstrap ON 2nd derivative';
-    @(a,v,R,tol) bootstrap_derivativefree(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'Bootstrap ON derivative-free';
+    @optimistic_newton, 'PN';
+    @(a,v,R,tol) bootstrap_first_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'PN-T1';
+    @(a,v,R,tol) bootstrap_wrong_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'PN-IMP';
+    @(a,v,R,tol) bootstrap_second_derivative(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'PN-T2';
+    @(a,v,R,tol) bootstrap_derivativefree(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'PN-EXT';
 %    @(a,v,R,tol) bootstrap_second_derivative_plus_previous(@optimistic_newton,a,v,R,tol,maxit,relspeed), 'Bootstrap ON derivative+prev';
-    @(a,v,R,tol) bootstrap_first_derivative(@newton,a,v,R,tol,maxit,relspeed), 'Bootstrap N 1st derivative';
-    @(a,v,R,tol) bootstrap_wrong_derivative(@newton,a,v,R,tol,maxit,relspeed), 'Bootstrap N wrong 1st';
-    @(a,v,R,tol) bootstrap_second_derivative(@newton,a,v,R,tol,maxit,relspeed), 'Bootstrap N 2nd derivative';
-    @(a,v,R,tol) bootstrap_derivativefree(@newton,a,v,R,tol,maxit,relspeed), 'Bootstrap N derivative-free';
+    @(a,v,R,tol) bootstrap_first_derivative(@newton,a,v,R,tol,maxit,relspeed), 'N-T1';
+    @(a,v,R,tol) bootstrap_wrong_derivative(@newton,a,v,R,tol,maxit,relspeed), 'N-IMP';
+    @(a,v,R,tol) bootstrap_second_derivative(@newton,a,v,R,tol,maxit,relspeed), 'N-T2';
+    @(a,v,R,tol) bootstrap_derivativefree(@newton,a,v,R,tol,maxit,relspeed), 'N-EXT';
     };
 
 [times, iters, results] = try_all_methods(0.9, methods(:,1));
@@ -33,18 +33,19 @@ set(0,'DefaultAxesFontSize',18);
 set(0,'DefaultLegendFontSize',16);
 set(0,'DefaultLineLineWidth',2);
 set(0,'DefaultLineMarkerSize', 10);
-set(0,'defaultaxeslinestyleorder',{'--',':','-.'})
+set(0,'defaultaxescolororder',[0 0 0; 0 0 0.8; 1 0 0; 0 1 0]);
+set(0,'defaultaxeslinestyleorder',{'-','--',':','-.'})
 
 %semilogy(times_true(order,:)); legend(methods(:,2))
 
 subplot(1,2,1);
-title('iterations');
 perfprof(iters_true); legend(methods(:,2), 'Location', 'southeast');
 set(gca, 'XScale', 'log')
+title('Iterations');
 subplot(1,2,2);
-title('CPU time');
-perfprof(times_true); legend(methods(:,2), 'Location', 'southeast');
+perfprof(times_true); % legend(methods(:,2), 'Location', 'southeast');
 set(gca, 'XScale', 'log')
+title('CPU time');
 
 legstring = join(methods(:,2),'\t'); legstring = legstring{1};
 legstring = strrep(legstring, ' ', '_');
